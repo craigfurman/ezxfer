47c4cc3a51504997a8eb9ace0a3bbbf1cc254903
Craig Furman <cfurman@pivotal.io>
2016-10-03 17:18:07 +0100

    refactor creation of logger

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 )
 
 func main() {
-	logger := log.New(os.Stdout, "[ezxfer] ", log.LstdFlags)
-
 	file := flag.String("file", "", "")
 	dstHost := flag.String("dstHost", "", "")
 	dstPort := flag.Int("dstPort", 0, "")
@@ -21,7 +19,7 @@ func main() {
 	flag.Parse()
 
 	if *serverPort != 0 {
-		logger.SetPrefix("[ezxfer server] ")
+		logger := createLogger("[ezxfer server] ")
 		logger.Println("-serveOnPort is set, starting in server mode")
 		srv := server.Server{Port: *serverPort, Logger: logger}
 		if err := srv.ServeTCP(); err != nil {
@@ -30,11 +28,16 @@ func main() {
 		}
 	}
 
-	logger.Println("will transfer file...")
+	logger := createLogger("[ezxfer] ")
+	logger.Printf("will transfer file %s to %s:%d...\n", *file, *dstHost, *dstPort)
 	if err := client.SendFile(*file, fmt.Sprintf("%s:%d", *dstHost, *dstPort)); err != nil {
 		logger.Println(err)
 		os.Exit(1)
 	}
 
 	logger.Println("done!")
+}
+
+func createLogger(prefix string) *log.Logger {
+	return log.New(os.Stdout, prefix, log.LstdFlags)
 }
